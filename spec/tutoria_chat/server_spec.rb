b30e9f735aaa861554455dd255272b0090f7f100
mleszcz <fryk@tlen.pl>
2011-07-26 18:49:46 +0000

    server#serve first test

--- spec/tutoria_chat/server_spec.rb
+++ spec/tutoria_chat/server_spec.rb
@@ -2,8 +2,17 @@ require 'spec_helper'
 
 module TutoriaChat
   describe Server do
-    describe "#initialize" do
-      it "inits all the stuff"
+    let(:server) { TutoriaChat::Server.new }
+    let(:client_1) { TutoriaChat::Client.new }
+    let(:client_2) { TutoriaChat::Client.new }
+
+    describe "#serve" do
+      it "stores client's sockets" do
+        server.should have(0).clients
+        expect {
+          client_1.start
+        }.to change{ server.clients.count }.to(1)
+      end
     end
   end
 end
