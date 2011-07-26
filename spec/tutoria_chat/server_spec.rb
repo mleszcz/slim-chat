4f587bec41d69c8d5672606bc29c8dc2d6717a80
mleszcz <fryk@tlen.pl>
2011-07-26 19:18:21 +0000

    unfinished tests

--- spec/tutoria_chat/server_spec.rb
+++ spec/tutoria_chat/server_spec.rb
@@ -4,14 +4,26 @@ module TutoriaChat
   describe Server do
     let(:server) { TutoriaChat::Server.new }
     let(:client_1) { TutoriaChat::Client.new }
-    let(:client_2) { TutoriaChat::Client.new }
+    let(:client_1) { TutoriaChat::Client.new }
+
+    before(:each) do
+      server.start
+    end
+
+    after(:each) do
+      server.stop
+    end
 
     describe "#serve" do
       it "stores client's sockets" do
         server.should have(0).clients
-        expect {
-          client_1.start
-        }.to change{ server.clients.count }.to(1)
+        client_1.stub(:get_line).and_return('hi there!','bye')
+
+        # expectations
+#        server.should_receive(:log).with("Message received(hi there)").once
+
+        # lunch
+        client_1.start
       end
     end
   end
